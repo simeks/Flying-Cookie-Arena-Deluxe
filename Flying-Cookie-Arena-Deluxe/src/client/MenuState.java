5e04b850f3accf7c87caea6ec9a6f325b2ddd063
raxo <oskar.klang@gmail.com>
2014-05-01 21:34:24 +0200

    Scroll panel bug fix

--- Flying-Cookie-Arena-Deluxe/src/client/MenuState.java
+++ Flying-Cookie-Arena-Deluxe/src/client/MenuState.java
@@ -8,12 +8,16 @@ import com.jme3.input.InputManager;
 import com.jme3.niftygui.NiftyJmeDisplay;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.builder.EffectBuilder;
 import de.lessvoid.nifty.builder.LayerBuilder;
 import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.builder.StyleBuilder;
 import de.lessvoid.nifty.builder.TextBuilder;
+import de.lessvoid.nifty.controls.Label;
 import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.controls.label.builder.LabelBuilder;
 import de.lessvoid.nifty.controls.scrollpanel.builder.ScrollPanelBuilder;
 import de.lessvoid.nifty.controls.textfield.builder.TextFieldBuilder;
 import de.lessvoid.nifty.elements.Element;
@@ -56,7 +60,8 @@ public class MenuState implements GameState {
 	public void updateServerList(Vector<String> servers, String nextUpdate) {
 		Nifty nifty = niftyDisplay.getNifty();
 		Screen screen = nifty.getCurrentScreen();
-		Element serverListPanel = screen.findElementByName("ServerListList");
+		Element serverListPanel = screen.findElementByName("ServerListList2");
+		
 		// add new
 		for(final String server : servers) {
 			if(!serversInList.contains(server)) {
@@ -67,7 +72,12 @@ public class MenuState implements GameState {
 						childLayoutCenter();
 						visibleToMouse(true);
 						interactOnClick("joinGameLobby(" + server + ")");
-						// wrap(true);
+						marginTop(String.valueOf(10*(serversInList.indexOf(server))+1)+"%");
+						height("10%");
+						width("100%");
+						textHAlignLeft();
+						valignTop();
+						alignLeft();
 					}}.build(nifty, screen, serverListPanel)
 				);
 				screen.layoutLayers();
@@ -82,7 +92,7 @@ public class MenuState implements GameState {
 			}
 		}
 		
-		screen.findNiftyControl("ServerListStatus", TextField.class).setText("Update interval is "+nextUpdate);
+		screen.findNiftyControl("ServerListStatus", Label.class).setText("Update interval is "+nextUpdate);
 	}
 	
 	// @brief callback from join button
@@ -134,13 +144,53 @@ public class MenuState implements GameState {
 	    nifty.loadStyleFile("nifty-default-styles.xml");
 	    nifty.loadControlFile("nifty-default-controls.xml");
 	 
+	    // issue #609.. http://hub.jmonkeyengine.org/forum/topic/nifty-labels-inside-a-scrollpanel-control/
+	    new StyleBuilder() {{
+	    	id("my-scrollpanel#scrollpanel");
+	    	backgroundColor("#ffff");
+	    	onActiveEffect(new EffectBuilder("ImageOverlay") {{
+				name("imageOverlay");
+				overlay(true);
+				filename("blackborder.png");
+				imageMode("resize:1,30,1,1,1,30,1,1,1,30,1,1");
+				post(true);
+			}});
+    	} }.build(nifty);
+    	
+    	// remove horizontal scroll
+    	new StyleBuilder() {{
+	    	id("nifty-horizontal-scrollbar#panel");
+	    	height("0px");
+	    	width("0px");
+	    	childLayoutHorizontal();
+    	}}.build(nifty);
+    	new StyleBuilder() {{
+	    	id("nifty-horizontal-scrollbar#left");
+	    	height("0px");
+	    	width("0px");
+    	}}.build(nifty);
+    	new StyleBuilder() {{
+	    	id("nifty-horizontal-scrollbar#right");
+	    	height("0px");
+	    	width("0px");
+    	}}.build(nifty);
+    	new StyleBuilder() {{
+    		childLayoutAbsolute();
+	    	id("nifty-horizontal-scrollbar#background");
+	    	height("0px");
+	    	width("0px");
+    	}}.build(nifty);
+    	new StyleBuilder() {{
+	    	id("nifty-horizontal-scrollbar#position");
+	    	height("0px");
+	    	width("0px");
+    	}}.build(nifty);
+	    
 	    final MenuState state = this;
-	    
-	    
 	    nifty.addScreen("ServerListScreen", new ScreenBuilder("Nifty Screen") {{
 	    	
-	    	 controller(new client.MyScreenController(state));
-	 
+	    	controller(new client.MyScreenController(state));
+	    	
 	        layer(new LayerBuilder("ServerListLayer") {{
 	        	childLayoutVertical();
 	        	backgroundColor("#ccc");
@@ -164,23 +214,33 @@ public class MenuState implements GameState {
     				height("70%");
     				width("100%");
     				
-    				text(new TextBuilder("ServerListStatus") {{
+    				control(new LabelBuilder("ServerListStatus") {{
 	               		alignRight();
 	               		valignTop();
 	        			text("Loading... ");
 	               		color("#000");
 	       				font("Interface/Fonts/Default.fnt");
 	       				height("10%");
-	       				width("100%");
+	       				width("50%");
+	       				textHAlignRight();
+	       				marginRight("10px");
+	       				textVAlignBottom();
+	       				wrap(true);
 	                }});
-	               
+    				
 	               	control(new ScrollPanelBuilder("ServerListScroll") {{
 	               		alignCenter();
 	               		valignBottom();
 	       				height("90%");
 	       				width("100%");
+	       				style("my-scrollpanel");
+	       				
+	       				panel(new PanelBuilder("ServerListList2") {{
+	    	            	childLayoutCenter();
+	        				height("100%");
+	        				width("100%");
+	       				}});
                		}});
-	               
 	            }});
 	            
 	            panel(new PanelBuilder("ServerListButtonPanel") {{
