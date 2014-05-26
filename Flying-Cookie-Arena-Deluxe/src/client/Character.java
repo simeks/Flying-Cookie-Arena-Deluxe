a3f805a0eeeaf551279716a1bdc8951c7499c6cd
raxo <oskar.klang@gmail.com>
2014-05-26 22:37:57 +0200

    flag color, flag accumulate on character

--- Flying-Cookie-Arena-Deluxe/src/client/Character.java
+++ Flying-Cookie-Arena-Deluxe/src/client/Character.java
@@ -35,21 +35,25 @@ public class Character extends Entity {
 		}
 		
 		Iterator<Spatial> iter = ((Node) node).getChildren().iterator();
+		Flag highetstFlag = null;
 		while(iter.hasNext()) {
 			Spatial child = iter.next();
 			
 			if (child.getUserData("id") != null){
 				if(world.getEntity((Integer) child.getUserData("id")) instanceof Flag){
-
-					((Flag)world.getEntity((Integer) child.getUserData("id"))).editEntity();
-					((Flag)world.getEntity((Integer) child.getUserData("id"))).pickupFlag(character.getNode());
-
-					return;
+					Flag flag =((Flag)world.getEntity((Integer) child.getUserData("id")));
+					if(highetstFlag == null) {
+						highetstFlag = flag;
+					} else if(flag.getPosition().getY() > highetstFlag.getPosition().getY()) {
+						highetstFlag = flag;
+					}
 				}
-				
 			}
 		}
-
+		if(highetstFlag != null) {
+			highetstFlag.editEntity();
+			highetstFlag.pickupFlag(character.getNode());
+		}
 	}
 
 	public static final int MOVEMENT_DELAY = 500; // Delay i ms for convergence
